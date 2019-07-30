6273cda2c88aa53cc5c5c3c9a32ecc0a543841f6
root <root@p05798818m05805.cern.ch>
2019-07-30 15:51:39 +0200

    ceph/scripts: make rockdb optional for castor setup

--- ceph-volume/striped-osd-prepare.sh
+++ ceph-volume/striped-osd-prepare.sh
@@ -3,15 +3,21 @@
 [ -z "$DISKS" ] && DISKS=48
 lvols=$((100*2/$DISKS))
 
+[ -z "$ROCKSDB" ] && ROCKSDB=false
+
 ls -l $@ | awk '{ print $10 }' | \
 while read d1; read d2; do
-        echo ceph-volume lvm zap $d1
-        echo ceph-volume lvm zap $d2
-        vgname=ceph-`uuid -v4`
-        lvname=osd-`uuid -v4`
-        cname=cache-`uuid -v4`
-        echo vgcreate $vgname $d1 $d2
-        echo lvcreate -i 2 -l 100%FREE -n $lvname $vgname
+    echo ceph-volume lvm zap $d1
+    echo ceph-volume lvm zap $d2
+    vgname=ceph-`uuid -v4`
+    lvname=osd-`uuid -v4`
+    cname=cache-`uuid -v4`
+    echo vgcreate $vgname $d1 $d2
+    echo lvcreate -i 2 -l 100%FREE -n $lvname $vgname
+    if $ROCKSDB; then
         echo lvcreate -l $lvols%VG -n $cname cephrocks
         echo ceph-volume lvm create --bluestore --data $vgname/$lvname --block.db cephrocks/$cname
+    else
+        echo ceph-volume lvm create --bluestore --data $vgname/$lvname
+    fi
 done
