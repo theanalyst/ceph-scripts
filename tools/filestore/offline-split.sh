873caa17009674aea8c45037da36d81892c65780
Dan van der Ster <daniel.vanderster@cern.ch>
2019-09-23 11:54:15 +0200

    prime-filestore-dirs.sh: pool name in log file

--- tools/filestore/offline-split.sh
+++ tools/filestore/offline-split.sh
@@ -9,7 +9,7 @@ DIRS=`mount | grep ceph | grep xfs | awk '{print $3}'`
 for OSD in ${DIRS};
 do
   ID=`cat ${OSD}/whoami`
-  echo "sudo -H -u ceph bash -c 'ceph-objectstore-tool --debug --type=filestore --data-path ${OSD} --op apply-layout-settings --pool ${POOL} ' &> /var/log/ceph/offline-split.${ID}.volumes.log &"
+  echo "sudo -H -u ceph bash -c 'ceph-objectstore-tool --debug --type=filestore --data-path ${OSD} --op apply-layout-settings --pool ${POOL} ' &> /var/log/ceph/offline-split.${ID}.${POOL}.log &"
 done
 echo wait
 echo systemctl start ceph-osd.target
