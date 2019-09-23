a9057a7ea303bd34d0aac3868f11eba0ba0b0a6b
Dan van der Ster <daniel.vanderster@cern.ch>
2019-09-23 10:44:02 +0200

    offline-split.sh: POOL as param

--- tools/filestore/offline-split.sh
+++ tools/filestore/offline-split.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+POOL=$1
+
 echo killall ceph-osd
 echo systemctl stop ceph-osd.target
 echo sleep 10
@@ -7,13 +9,7 @@ DIRS=`mount | grep ceph | grep xfs | awk '{print $3}'`
 for OSD in ${DIRS};
 do
   ID=`cat ${OSD}/whoami`
-  echo "sudo -H -u ceph bash -c 'ceph-objectstore-tool --debug --type=filestore --data-path ${OSD} --op apply-layout-settings --pool volumes' &> /var/log/ceph/offline-split.${ID}.volumes.log &"
-done
-echo wait
-for OSD in ${DIRS};
-do
-  ID=`cat ${OSD}/whoami`
-  echo "sudo -H -u ceph bash -c 'ceph-objectstore-tool --debug --type=filestore --data-path ${OSD} --op apply-layout-settings --pool images' &> /var/log/ceph/offline-split.${ID}.images.log &"
+  echo "sudo -H -u ceph bash -c 'ceph-objectstore-tool --debug --type=filestore --data-path ${OSD} --op apply-layout-settings --pool ${POOL} ' &> /var/log/ceph/offline-split.${ID}.volumes.log &"
 done
 echo wait
 echo systemctl start ceph-osd.target
