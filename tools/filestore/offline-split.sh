7f79b7eb707409d97f130421bf2f8ba03e7ac30b
Dan van der Ster <daniel.vanderster@cern.ch>
2019-09-24 10:31:11 +0200

    offline-split: support multiple pools

--- tools/filestore/offline-split.sh
+++ tools/filestore/offline-split.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-POOL=$1
+shift
 
 echo killall ceph-osd
 echo systemctl stop ceph-osd.target
 echo sleep 10
 DIRS=`mount | grep ceph | grep xfs | awk '{print $3}'`
-for OSD in ${DIRS};
-do
-  ID=`cat ${OSD}/whoami`
-  echo "sudo -H -u ceph bash -c 'ceph-objectstore-tool --debug --type=filestore --data-path ${OSD} --op apply-layout-settings --pool ${POOL} ' &> /var/log/ceph/offline-split.${ID}.${POOL}.log &"
+while (( "$#" )); do
+  POOL=$1
+  for OSD in ${DIRS};
+  do
+    ID=`cat ${OSD}/whoami`
+    echo "sudo -H -u ceph bash -c 'ceph-objectstore-tool --debug --type=filestore --data-path ${OSD} --op apply-layout-settings --pool ${POOL} ' &> /var/log/ceph/offline-split.${ID}.${POOL}.log &"
+  done
+  echo wait
+  shift
 done
-echo wait
 echo systemctl start ceph-osd.target
