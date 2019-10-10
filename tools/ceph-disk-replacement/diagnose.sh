9dc4f21572fb6fdfb5004085af0d2247b76c0c06
Dan van der Ster <daniel.vanderster@cern.ch>
2019-10-10 13:22:38 +0200

    minor fix
    Signed-off-by: Dan van der Ster <daniel.vanderster@cern.ch>

--- tools/ceph-disk-replacement/diagnose.sh
+++ tools/ceph-disk-replacement/diagnose.sh
@@ -3,8 +3,6 @@
 PATIENT=`echo $HOSTNAME | cut -d"." -f1 `
 
 echo "Diagnosing patient: $PATIENT"
-
-echo "Generating ill osd list"
 for i in `ceph osd tree down | awk -v awkhost=$PATIENT 'BEGIN { out=0 } { if($0 ~ /host/ && out) {out=0} if(out) {print $0;} if($0 ~ awkhost) {out=1}; }' | grep -Eo "osd\.[0-9]+" | tr -d "[a-z\.]"`;
 do
   OSD=`echo "osd.$i"`;
@@ -17,7 +15,7 @@ do
   if [[ -z $DEV ]];
   then
     for i in `lsscsi | grep -Eo "/dev/sd[c-z]|/dev/sd[a-z][a-z]"`; do lvs -o +devices,tags | grep "$i" -q; if [[ $? -eq 1 ]]; then DEV=`echo $DEV $i`; fi; done
-    echo "-> $DEV seems unattached, replacement in progress?"
+    echo "$OSD: $DEV seems unattached, replacement in progress?"
   fi
 
   for i in `echo $DEV | grep -Eo "sd[a-z]+"`;
@@ -25,9 +23,7 @@ do
     dmesg -T | grep $i | grep -qi Error;
     if [[ $? -eq 0 ]];
     then
-      echo "- $OSD: bad drive $i"
+      echo "$OSD: bad drive $i"
     fi
   done
 done
-
-echo " "
