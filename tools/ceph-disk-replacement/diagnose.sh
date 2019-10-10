6fd1036b9d4dd45048b3043c73f5c7af739d6c88
root <root@p05972678e21448.cern.ch>
2019-10-10 12:09:48 +0200

    improvements
    Signed-off-by: root <root@p05972678e21448.cern.ch>

--- tools/ceph-disk-replacement/diagnose.sh
+++ tools/ceph-disk-replacement/diagnose.sh
@@ -5,7 +5,7 @@ PATIENT=`echo $HOSTNAME | cut -d"." -f1 `
 echo "Diagnosing patient: $PATIENT"
 
 echo "Generating ill osd list"
-for i in `ceph osd tree down | awk -v awkhost=$PATIENT 'BEGIN { out=0 } { if($0 ~ /rack/) {out=0} if(out) {print $0;} if($0 ~ awkhost) {out=1}; }' | grep -Eo "osd\.[0-9]+" | tr -d "[a-z\.]"`;
+for i in `ceph osd tree down | awk -v awkhost=$PATIENT 'BEGIN { out=0 } { if($0 ~ /host/ && out) {out=0} if(out) {print $0;} if($0 ~ awkhost) {out=1}; }' | grep -Eo "osd\.[0-9]+" | tr -d "[a-z\.]"`;
 do
   OSD=`echo "osd.$i"`;
   DEV=""
@@ -16,7 +16,6 @@ do
 
   if [[ -z $DEV ]];
   then
-    echo "No device found"
     for i in `lsscsi | grep -Eo "/dev/sd[c-z]|/dev/sd[a-z][a-z]"`; do lvs -o +devices,tags | grep "$i" -q; if [[ $? -eq 1 ]]; then DEV=`echo $DEV $i`; fi; done
     echo "-> $DEV seems unattached, replacement in progress?"
   fi
