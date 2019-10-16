7b346f1882e92c01a0905516c151b9967114b305
root <root@p06253939f99921.cern.ch>
2019-10-16 17:16:21 +0200

    improvement on lost osd script
    Signed-off-by: root <root@p06253939f99921.cern.ch>

--- tools/ceph-disk-replacement/lost-osds.sh
+++ tools/ceph-disk-replacement/lost-osds.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
 
-for i in `ceph-volume inventory | grep -E "/dev/sd[a=z]?[a-z]"`;
-do
- echo "-> $i"
-done
-
+AVAILDRIVES=`ceph-volume inventory | awk '{ if( $1 ~ /\/dev\/sd[a-z]?[a-z]/ ) { if ( $5 ~ /True/ ) { if ( $4 ~ /True/ ) { print $0" HDD"} else { print $0" SSD"}} } }'`
+echo $AVAILDRIVES
