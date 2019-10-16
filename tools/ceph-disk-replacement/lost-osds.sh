65d000c72bbd0893ef25758b6940c80546d8602c
root <root@cephbeesly-mon-2a00f134e5.cern.ch>
2019-10-16 14:46:50 +0200

    lost osd improvement
    Signed-off-by: root <root@cephbeesly-mon-2a00f134e5.cern.ch>

--- tools/ceph-disk-replacement/lost-osds.sh
+++ tools/ceph-disk-replacement/lost-osds.sh
@@ -1,19 +1,8 @@
 #! /bin/bash
 
 
-
-#ceph osd tree | grep host | awk '{ printf "%3.0f ", $2; print $4 }'
-#ceph osd tree | grep host | awk '{ printf "%3.0f ", $2; }' | uniq
-
-
-# Identify disk layouts
-
-
-# Identify which hosts have missing drives
-ceph osd tree | awk '{ if( $0 ~ /host/ ) { print c; c = 0; printf $4" "};  if( $0 ~ /osd/) { c = c+1 }  } END { print c }' > /tmp/tmpfile.hostlist.log
-
-echo "done"
-for i in `grep -v "24$" /tmpfile.hostlist.log`;
+for i in `ceph-volume inventory | grep -E "/dev/sd[a=z]?[a-z]"`;
 do
-    echo "-- $i --"
+ echo "-> $i"
 done
+
