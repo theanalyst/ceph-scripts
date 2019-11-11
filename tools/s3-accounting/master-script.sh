1c3e691b28dfa03de12eb6ea4c639c9262435e37
Julien COLLET <julien.collet@cern.ch>
2019-11-11 11:59:15 +0100

    fix
    Signed-off-by: Julien COLLET <julien.collet@cern.ch>

---
+++ tools/s3-accounting/master-script.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+
+export OS_PROJECT_NAME=Services
+
+OUTFILE="s3-accounting-`date '+%Y-%m-%d'`.log"
+rm $OUTFILE
+
+
+#ssh cephadm /root/ceph-scripts/tools/s3-accounting/s3-accounting-full.py > $FILENAME
+FILENAME="s3accfull.out"
+
+while read -r line; 
+do 
+  prid=`echo $line | grep -Eo "[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}"`; 
+  if [ ! -z $prid ]; 
+  then 
+    echo -n $line" " >> $OUTFILE; 
+  ./s3-user-to-accounting-unit.py $prid >> $OUTFILE
+  fi;
+done < $FILENAME
+
+while read -r line; 
+do 
+  prid=`echo $line | grep -Eo "[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}"`; 
+  if [ -z  $prid ]; 
+  then 
+    id=`echo $line | grep -Eo "\(.*\)" | tr -d "()"`;
+    echo -n $line >> $OUTFILE
+    ./get-user-dept-group-info-by-email.sh `echo $line | grep  -Eo "[a-z0-9\.-]*@.*$"` | awk '{print ", "$1"/"$2}' >> $OUTFILE
+  fi;
+done < $FILENAME
+
+
+s3cmd put $OUTFILE s3://s3-accounting-files
+#
+# push $FILENAME to S3
+#
