c6bd47be88f50d35da8a2ac213a9c00434e43600
Julien COLLET <julien.collet@cern.ch>
2019-11-20 14:03:57 +0100

    add quota treshold mechanism

--- tools/s3-accounting/master-script.sh
+++ tools/s3-accounting/master-script.sh
@@ -4,10 +4,12 @@
 export OS_PROJECT_NAME=Services
 
 OUTFILE="s3-accounting-`date '+%Y-%m-%d'`.log"
-rm $OUTFILE
-
-
+PRVFILE="s3-accounting-`date -d "yesterday" '+%Y-%m-%d'`.log"
+TRESHOLD=$1
 FILENAME="/tmp/s3accounting.tmp.log"
+
+echo -n "" > $OUTFILE
+
 ssh cephadm /root/ceph-scripts/tools/s3-accounting/s3-accounting-full.py > $FILENAME
 
 while read -r line; 
@@ -27,8 +29,34 @@ do
   then 
     id=`echo $line | grep -Eo "\(.*\)" | tr -d "()"`;
     echo -n $line >> $OUTFILE
-    ./get-user-dept-group-info-by-email.sh `echo $line | grep  -Eo "[a-z0-9\.-]*@.*$"` | awk '{print ", "$1"/"$2}' >> $OUTFILE
+    ./get-user-dept-group-info-by-email.sh `echo $line | grep  -Eo "[a-z0-9\.-]*@.*$" | tr -d ","` | awk '{print " "$1"/"$2}' >> $OUTFILE
   fi;
 done < $FILENAME
 
 s3cmd put $OUTFILE s3://s3-accounting-files
+s3cmd get s3://s3-accounting-files/$PRVFILE  
+
+while read -r line;
+do
+  uid=`echo $line | grep -Eo "\(.*\)"`;
+
+  if [ -z $uid ];
+  then
+    echo "-> $line"
+  else
+    prv=`grep "$uid" $PRVFILE | grep -Eo ", [0-9.]+ percent full" | tr -d "[a-z ,]"`
+    act=`echo $line | grep -E "$uid" | grep -Eo ", [0-9.]+ percent full" | tr -d "[a-z ,]"`
+
+    if (( $(echo "$act > $TRESHOLD" | bc -l) )) && (( $(echo "$prv < $TRESHOLD" | bc -l) ))  ;
+    then
+      echo "`echo $uid | tr -d "()"`: is $act, was $prv -> generate email"
+    fi
+  fi
+done < $OUTFILE
+
+# clean
+rm $PRVFILE
+rm $OUTFILE
+
+
+
