677260c3040c9b3416823b74bdaaab15c4670dbb
Julien COLLET <julien.collet@cern.ch>
2019-12-10 16:59:03 +0100

    ceph-disk-replacement: clean-up

--- tools/ceph-disk-replacement/lost-osds.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-
-AVAILDRIVES=`ceph-volume inventory | awk '{ if( $1 ~ /\/dev\/sd[a-z]?[a-z]/ ) { if ( $5 ~ /True/ ) { if ( $4 ~ /True/ ) { print $0" HDD"} else { print $0" SSD"}} } }'`
-echo $AVAILDRIVES
