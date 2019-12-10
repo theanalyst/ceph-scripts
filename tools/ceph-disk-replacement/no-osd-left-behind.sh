782573eb662c243c1ca7f4afac08c300cc9f5da1
Julien COLLET <julien.collet@cern.ch>
2019-12-10 16:55:41 +0100

    no-osd-left-behind: improvements

--- tools/ceph-disk-replacement/no-osd-left-behind.sh
+++ tools/ceph-disk-replacement/no-osd-left-behind.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
-while [[ $# -gt 0 ]]
-do
-    key="$1"
-
-    case "$key" in
-        -s) 
-        shift; 
-        SUMMARY=1;
-        ;;
-
-    esac
-done
-
-echo "Scanning ceph/`cat /etc/motd | grep -Eo "ceph/.*/mon" | cut -d/ -f2`"
-
-for i in `ceph osd tree down | grep -E "host" | cut -d"p" -f2`; 
+for i in `ceph osd tree | grep host | grep -Eo "p.*"`; 
 do 
-  ssh p"$i" /root/ceph-scripts/tools/ceph-disk-replacement/diagnose.sh; 
+  ssh $i ceph-scripts/tools/ceph-disk-replacement/list-available-drives.sh; 
 done
 
-
-if [[ ! -z $SUMMARY ]];
-then
-    FAILEDOSD=`ceph osd tree down | grep osd | wc -l`;
-
-    echo "Summary for ceph/`cat /etc/motd | grep -Eo "ceph/.*/mon" | cut -d/ -f2`:"
-    echo "Number of down OSDs: $FAILEDOSD"
-fi
+#while [[ $# -gt 0 ]]
+#do
+#    key="$1"
+#
+#    case "$key" in
+#        -s) 
+#        shift; 
+#        SUMMARY=1;
+#        ;;
+#
+#    esac
+#done
+#
+#echo "Scanning ceph/`cat /etc/motd | grep -Eo "ceph/.*/mon" | cut -d/ -f2`"
+#
+#for i in `ceph osd tree down | grep -E "host" | cut -d"p" -f2`; 
+#do 
+#  ssh p"$i" /root/ceph-scripts/tools/ceph-disk-replacement/diagnose.sh; 
+#done
+#
+#
+#if [[ ! -z $SUMMARY ]];
+#then
+#    FAILEDOSD=`ceph osd tree down | grep osd | wc -l`;
+#
+#    echo "Summary for ceph/`cat /etc/motd | grep -Eo "ceph/.*/mon" | cut -d/ -f2`:"
+#    echo "Number of down OSDs: $FAILEDOSD"
+#fi
