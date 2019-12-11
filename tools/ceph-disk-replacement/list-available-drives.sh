d4b01db1357d8ea427e14c556214971a3d804632
root <root@p05798818m05805.cern.ch>
2019-12-11 16:56:18 +0100

    use json output of the ceph-volume inventory cmd

--- tools/ceph-disk-replacement/list-available-drives.sh
+++ tools/ceph-disk-replacement/list-available-drives.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-drives=`ceph-volume inventory | awk '{ if( $4 == "True" && $5 == "True") print $0}' | grep -v "/dev/m" `
-
+#drives=`ceph-volume inventory | awk '{ if( $4 == "True" && $5 == "True") print $0}' | grep -v "/dev/m" `
+drives=`ceph-volume inventory --format json | jq '.[] | select(.available)'`
 
 if [[ $drives -ne "" ]];
 then
-  echo " $HOSTNAME: "
+  echo "$HOSTNAME: "
   echo "$drives"
   echo ""
 fi
