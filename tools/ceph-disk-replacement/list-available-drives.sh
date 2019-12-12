a0af566fd2b5196ee2b48d74c09f56736ce44181
root <root@p05798818m05805.cern.ch>
2019-12-12 10:57:28 +0100

    add hostname to json output

--- tools/ceph-disk-replacement/list-available-drives.sh
+++ tools/ceph-disk-replacement/list-available-drives.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
 #drives=`ceph-volume inventory | awk '{ if( $4 == "True" && $5 == "True") print $0}' | grep -v "/dev/m" `
-drives=`ceph-volume inventory --format json | jq '.[] | select(.available)'`
+drives=`ceph-volume inventory --format json | jq --arg host ${HOSTNAME} '.[] | select(.available) | . += { hostname:$host}'`
 
 if [[ $drives -ne "" ]];
 then
-  echo "$HOSTNAME: "
   echo "$drives"
-  echo ""
 fi
