ee374edc92997b0841e8be8774ef4d29004d47a7
Julien COLLET <julien.collet@cern.ch>
2019-12-12 14:22:05 +0100

    remove useless code

--- tools/ceph-disk-replacement/list-available-drives.sh
+++ tools/ceph-disk-replacement/list-available-drives.sh
@@ -2,8 +2,4 @@
 
 #drives=`ceph-volume inventory | awk '{ if( $4 == "True" && $5 == "True") print $0}' | grep -v "/dev/m" `
 drives=`ceph-volume inventory --format json | jq --arg host ${HOSTNAME} '.[] | select(.available) | . += { hostname:$host}'`
-
-if [[ $drives -ne "" ]];
-then
-  echo "$drives"
-fi
+echo "$drives"
