32889861a282e81eb3a1cdd459c0cc371acdbd48
Julien Collet <julien.collet@cern.ch>
2020-01-06 16:21:49 +0100

    convert to python3

--- tools/s3-scanner/cern-s3-scanner.py
+++ tools/s3-scanner/cern-s3-scanner.py
@@ -25,20 +25,20 @@ def checkBucket(bName, mode='default', triesLeft=2):
     
 
     if r.status_code == 200:
-        print bUrl + ' (',r.status_code,')'
+        print(bUrl + ' (',r.status_code,')')
         return True
     elif r.status_code == 403:
         if mode == 'listall':
-            print bUrl + ' (',r.status_code,')'
+            print(bUrl + ' (',r.status_code,')')
         return False
     elif r.status_code == 404: 
         if mode == 'listall':
-            print bUrl + ' (',r.status_code,')'
+            print(bUrl + ' (',r.status_code,')')
         return False
     elif r.status_code == 503:
         return checkBucketWithoutCreds(bucketName, mode, triesLeft-1)
     else:
-        print bUrl + ' (',r.status_code,') --> Unhandled status code'
+        print(bUrl + ' (',r.status_code,') --> Unhandled status code')
         return False
 
 def exploreBucket(bName, mode='default'):
@@ -51,7 +51,7 @@ def exploreBucket(bName, mode='default'):
         objString = bUrl + '/' + re.findall('<Key>(.+?)</Key>', content)[0] + ' (' + re.findall('<ID>(.+?)</ID>', content)[0] + ')' #+ ' ' + re.findall('<DisplayName>(.+?)</DisplayName>', content)[0] 
         objR = requests.head(bUrl + '/' + re.findall('<Key>(.+?)</Key>', content)[0])
         if objR.status_code == 200 or mode == 'listopen':   
-            print '  [', objR.status_code, '] ' + objString
+            print('  [', objR.status_code, '] ' + objString)
 
 parser = argparse.ArgumentParser(description='# CERN s3 Scanner - simple s3 bucket scanner\n'
                                              '#\n'
@@ -61,8 +61,8 @@ parser = argparse.ArgumentParser(description='# CERN s3 Scanner - simple s3 buck
                                  prog='cern-s3-scanner')
 
 # Declare arguments
-parser.add_argument('-o', '--out-file', dest='outFile', default='./buckets.txt',
-                    help='Name of file to save the successfully checked buckets in (Default: buckets.txt)')
+#parser.add_argument('-o', '--out-file', dest='outFile', default='./buckets.txt',
+#                    help='Output file')
 parser.add_argument('-m', '--mode', dest='mode', default='default',
         help='Scan mode: instead of printing only publicly accessible resources:  \n - listall, dump everything\n - listopen, dump open buckets and their contents\n - bucketonly, print only bucket, not their content ')
 parser.add_argument('buckets', help='Name of text file containing buckets to check')
@@ -72,12 +72,7 @@ args = parser.parse_args()
 
 #mode selection: 
 
-if path.isfile(args.buckets):
-    with open(args.buckets, 'r') as f:
-        for line in f:
-            line = line.rstrip()
-            # check buckets
-else:
+if not path.isfile(args.buckets):
     if checkBucket(args.buckets, args.mode): 
         if args.mode != 'bucketonly': 
             exploreBucket(args.buckets, args.mode)
