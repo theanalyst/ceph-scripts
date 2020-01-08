f7c14c27b35bd12396e4a5fd5f89b5df7bf08443
Dan van der Ster <daniel.vanderster@cern.ch>
2020-01-08 15:18:05 +0100

    striped-osd-prepare: prepare for ceph/gabe hosts cephrocks

--- ceph-volume/striped-osd-prepare.sh
+++ ceph-volume/striped-osd-prepare.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-[ -z "$DISKS" ] && DISKS=48
-lvols=$((100*2/$DISKS))
+# Check if cephrocks VG exists
+vgs cephrocks &> /dev/null && ROCKSDB=true || ROCKSDB=false
 
-[ -z "$ROCKSDB" ] && ROCKSDB=false
+# minimum must be (256MiB+2560MiB+25600MiB+1GiB)=28.75GiB
+# bigger than that doesn't help (except during compaction)
+DBSIZE="29GiB"
 
 ls -l $@ | awk '{ print $10 }' | \
 while read d1; read d2; do
     echo ceph-volume lvm zap $d1
     echo ceph-volume lvm zap $d2
-    vgname=ceph-`uuid -v4`
-    lvname=osd-`uuid -v4`
-    cname=cache-`uuid -v4`
+    vgname=ceph-block-`uuid -v4`
+    lvname=osd-block-`uuid -v4`
+    dbname=osd-block-db-`uuid -v4`
     echo vgcreate $vgname $d1 $d2
     echo lvcreate -i 2 -l 100%FREE -n $lvname $vgname
     if $ROCKSDB; then
-        echo lvcreate -l $lvols%VG -n $cname cephrocks
-        echo ceph-volume lvm create --bluestore --data $vgname/$lvname --block.db cephrocks/$cname
+        echo lvcreate -L $DBSIZE -n $dbname cephrocks
+        echo ceph-volume lvm create --bluestore --data $vgname/$lvname --block.db cephrocks/$dbname
     else
         echo ceph-volume lvm create --bluestore --data $vgname/$lvname
     fi
