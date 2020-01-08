5be99262e3f5405642188a69c201e14e0bbca9f5
Julien Collet <julien.collet@cern.ch>
2020-01-08 17:09:41 +0100

    cern-s3-scanner: add getBucketOwner

--- tools/s3-scanner/cern-s3-scanner.py
+++ tools/s3-scanner/cern-s3-scanner.py
@@ -11,9 +11,11 @@ import argparse
 import sys
 import requests
 import re
+import json
+import subprocess
 
 
-def checkBucket(bName, mode='default', triesLeft=2):
+def checkBucket(bName, mode='default', outFile=sys.stdout, triesLeft=2):
     """ Simple check using a request, pretty much like in S3scanner if you ask me
     - bName: bucket name """
 
@@ -21,25 +23,37 @@ def checkBucket(bName, mode='default', triesLeft=2):
         return False
 
     bUrl = 'https://'+ bName + '.s3.cern.ch'
-    r = requests.head(bUrl)
-    
+
+    try:
+        r = requests.head(bUrl)
+    except:
+        print('Error requesting the following Url: ',bUrl,file=outFile)
+        exit(-1)
 
     if r.status_code == 200:
-        print(bUrl + ' (',r.status_code,')')
+        print(getBucketOwner(bName),': ',bUrl + ' (',r.status_code,')',file=outFile)
         return True
     elif r.status_code == 403:
         if mode == 'listall':
-            print(bUrl + ' (',r.status_code,')')
+            print(bUrl + ' (',r.status_code,')',file=outFile)
         return False
     elif r.status_code == 404: 
         if mode == 'listall':
-            print(bUrl + ' (',r.status_code,')')
+            print(bUrl + ' (',r.status_code,')',file=outFile)
         return False
     elif r.status_code == 503:
-        return checkBucketWithoutCreds(bucketName, mode, triesLeft-1)
+        return checkBucketWithoutCreds(bucketName, mode, outFile, triesLeft-1)
     else:
-        print(bUrl + ' (',r.status_code,') --> Unhandled status code')
+        print(bUrl + ' (',r.status_code,') --> Unhandled status code',file=sys.stderr)
         return False
+
+def getBucketOwner(bName):
+    try:
+        info = json.loads(subprocess.getoutput('radosgw-admin --cluster=gabe metadata get bucket:',bName))
+        return info['data']['owner']
+    except:
+        print('Couldn\'t reach cephgabe',file=sys.stdout)
+        return 'OwnerNotFound'
 
 def exploreBucket(bName, mode='default'):
     """ Explore bucket (if possible) and show publicly accessible objects 
@@ -47,11 +61,11 @@ def exploreBucket(bName, mode='default'):
 
     bUrl = 'https://'+ bName + '.s3.cern.ch'
     r = requests.get(bUrl)
-    for content in re.findall('<Contents>(.+?)</Contents>', r.content, re.MULTILINE):
+    for content in re.findall('<Contents>(.+?)</Contents>', str(r.content), re.MULTILINE):
         objString = bUrl + '/' + re.findall('<Key>(.+?)</Key>', content)[0] + ' (' + re.findall('<ID>(.+?)</ID>', content)[0] + ')' #+ ' ' + re.findall('<DisplayName>(.+?)</DisplayName>', content)[0] 
         objR = requests.head(bUrl + '/' + re.findall('<Key>(.+?)</Key>', content)[0])
         if objR.status_code == 200 or mode == 'listopen':   
-            print('  [', objR.status_code, '] ' + objString)
+            print('  [', objR.status_code, '] ' + objString,file=outFile)
 
 parser = argparse.ArgumentParser(description='# CERN s3 Scanner - simple s3 bucket scanner\n'
                                              '#\n'
@@ -61,19 +75,17 @@ parser = argparse.ArgumentParser(description='# CERN s3 Scanner - simple s3 buck
                                  prog='cern-s3-scanner')
 
 # Declare arguments
-#parser.add_argument('-o', '--out-file', dest='outFile', default='./buckets.txt',
-#                    help='Output file')
+parser.add_argument('-o', '--out-file', dest='outFile', default=sys.stdout,
+                    help='Output file')
 parser.add_argument('-m', '--mode', dest='mode', default='default',
         help='Scan mode: instead of printing only publicly accessible resources:  \n - listall, dump everything\n - listopen, dump open buckets and their contents\n - bucketonly, print only bucket, not their content ')
 parser.add_argument('buckets', help='Name of text file containing buckets to check')
 
+
 args = parser.parse_args()
 
-
-#mode selection: 
-
 if not path.isfile(args.buckets):
-    if checkBucket(args.buckets, args.mode): 
+    if checkBucket(args.buckets, args.mode, args.outFile): 
         if args.mode != 'bucketonly': 
             exploreBucket(args.buckets, args.mode)
 
