8b256f643fd9e6ce804d8b86901b28fc36a044b3
Dan van der Ster <daniel.vanderster@cern.ch>
2020-01-09 17:35:18 +0100

    CEPH-801: convert get-s3-user-stats.py to python3

--- tools/s3-accounting/get-s3-user-stats.py
+++ tools/s3-accounting/get-s3-user-stats.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python -u
+#!/usr/bin/python3 -u
 
 from __future__ import division
 
-import json, commands, ldap, smtplib, sys
+import json, subprocess, smtplib, sys
 
 
 def sizeof_fmt(num, suffix='B'):
@@ -12,19 +12,19 @@ def sizeof_fmt(num, suffix='B'):
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
-users = sys.stdin.readlines(); # json.loads(commands.getoutput('radosgw-admin --cluster=gabe user list'))
+users = sys.stdin.readlines(); # json.loads(subprocess.getoutput('radosgw-admin --cluster=gabe user list'))
 
 for uid in users:
-    info = json.loads(commands.getoutput('radosgw-admin --cluster=gabe user info --uid=%s' % uid.strip('\n')))
+    info = json.loads(subprocess.getoutput('radosgw-admin --cluster=gabe user info --uid=%s' % uid.strip('\n')))
     if info['user_quota']['max_size_kb'] > 1:
         try:
-            stats = json.loads(commands.getoutput('radosgw-admin --cluster=gabe user stats --uid=%s' % uid.strip('\n')))['stats']
+            stats = json.loads(subprocess.getoutput('radosgw-admin --cluster=gabe user stats --uid=%s' % uid.strip('\n')))['stats']
         except:
             stats = {}
             stats['total_bytes'] = 0
             stats['total_entries'] = 0
 
-        buckets = json.loads(commands.getoutput('radosgw-admin --cluster=gabe bucket list --uid=%s' % uid.strip('\n')))
+        buckets = json.loads(subprocess.getoutput('radosgw-admin --cluster=gabe bucket list --uid=%s' % uid.strip('\n')))
         percentused = 100*stats['total_bytes']/info['user_quota']['max_size'];
 
-        print "%s (%s): %s quota, %s used, %.2f percent full,  %d buckets, %d objects, " % (info['display_name'], uid.strip('\n'), sizeof_fmt(info['user_quota']['max_size']), sizeof_fmt(stats['total_bytes']), percentused, len(buckets), stats['total_entries']) #, info['email'] if info['email'] != '' else 'none' )
+        print("%s (%s): %s quota, %s used, %.2f percent full,  %d buckets, %d objects, " % (info['display_name'], uid.strip('\n'), sizeof_fmt(info['user_quota']['max_size']), sizeof_fmt(stats['total_bytes']), percentused, len(buckets), stats['total_entries'])) #, info['email'] if info['email'] != '' else 'none' )
