a49d4050660f4285cfdd5420f476eb2f7d6eb781
Julien Collet <julien.collet@cern.ch>
2020-01-09 16:16:26 +0100

    ceph-disk-replacement: add generic roger checks

--- tools/ceph-disk-replacement/drain-osd.sh
+++ tools/ceph-disk-replacement/drain-osd.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
+if [[ `roger show $HOSTNAME  | jq .[].appstate | tr -d "\""` == "intervention" ]];
+then
+  echo "Operation cannot be performed. Machine currently in `roger show $HOSTNAME | jq '.[].appstate' | grep -Eo [a-Z]+` state (updated by: `roger show $HOSTNAME | jq '.[].updated_by'`) with the following message: `roger show $HOSTNAME | jq '.[].message'`";
+  if [[ `roger show $HOSTNAME  | jq .[].expires | tr -d "\""` ]];
+  then
+    echo "Disk replacement operations impossible until `roger show $HOSTNAME  | jq .[].expires`";
+  else
+    echo "No end-of-intervention date specified, contact ceph-admins";
+  fi
+  exit
+fi
+
+
 INITSTATE=`ceph health`
 FORCEMODE=0;
 VERBOSE=0
--- tools/ceph-disk-replacement/prepare-for-replacement.sh
+++ tools/ceph-disk-replacement/prepare-for-replacement.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
+if [[ `roger show $HOSTNAME  | jq .[].appstate | tr -d "\""` == "intervention" ]];
+then
+  echo "Operation cannot be performed. Machine currently in `roger show $HOSTNAME | jq '.[].appstate' | grep -Eo [a-Z]+` state (updated by: `roger show $HOSTNAME | jq '.[].updated_by'`) with the following message: `roger show $HOSTNAME | jq '.[].message'`";
+  if [[ `roger show $HOSTNAME  | jq .[].expires | tr -d "\""` ]];
+  then
+    echo "Disk replacement operations impossible until `roger show $HOSTNAME  | jq .[].expires`";
+  else
+    echo "No end-of-intervention date specified, contact ceph-admins";
+  fi
+  exit
+fi
+
+
 if [[ `cat /etc/motd | grep hostgroup | grep -Eo "ceph/[a-Z0-9/]+" | grep -c erin` -eq 1 ]];
 then
   CASTOR=1
