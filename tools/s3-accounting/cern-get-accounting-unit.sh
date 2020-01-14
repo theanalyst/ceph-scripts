be92d12fcf5687c32305a1225762fa84041216b2
Julien COLLET <julien.collet@cern.ch>
2020-01-14 16:55:21 +0100

    cern-get-accounting-unit: fix display of owner id

--- tools/s3-accounting/cern-get-accounting-unit.sh
+++ tools/s3-accounting/cern-get-accounting-unit.sh
@@ -34,14 +34,14 @@ userid=`echo $ID | sed -e 's/@.*//'`
 reply=`ldapsearch -xLLL -h xldap.cern.ch -p 389 -b "dc=cern,dc=ch" "(|(mail=$ID)(cn=$userid))"`
 
 serviceowner=`echo $reply | grep -Eo "managedBy: CN=[a-z0-9\-\+]*," | sed -e 's/.*CN=//' | tr -d ","`
-
 mail=`echo $reply | grep -Eo "mail: [a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+" | sed -e 's/.*: //'`
 
 if [ -z $serviceowner ];
 then
+  user=`echo $reply | grep -Eo "cn: [a-zA-Z0-9]+" | sed -e 's/cn: //'`;
   userdepgrp=`echo $reply | grep -Eo "department: [A-Z]+[/A-Z]?+" | sed -e 's/.*: //'`;
   userSection=`echo $reply | grep -Eo "cernSection: [A-Z]+" | sed -e 's/.*: /\//'`;
-  echo -n "$userid, "
+  echo -n "$user, "
 else
   replytoso=`ldapsearch -x -h xldap.cern.ch -p 389 -s base -b "cn=$serviceowner,ou=Users,ou=Organic Units,dc=cern,dc=ch"`
   userdepgrp=`echo $replytoso|  grep -Eo "department: [A-Z]+[/A-Z]?+" | sed -e 's/.*: //'`
