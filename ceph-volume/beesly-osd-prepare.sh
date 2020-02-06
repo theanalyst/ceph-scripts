5520daab9eb0503c33b7ea33f044c60022ce9f2a
tmourati <tmourati@thmour-lenovo.dyndns.cern.ch>
2020-02-06 11:58:09 +0100

    ceph-volume: delete unused stale scripts

--- ceph-volume/beesly-osd-prepare.sh
+++
@@ -1,5 +0,0 @@
-ceph-volume lvm zap /dev/sd[a-x] /dev/sda[a-d]
-ceph-volume lvm batch /dev/sd[a-f] /dev/sdaa --yes
-ceph-volume lvm batch /dev/sd[g-l] /dev/sdab --yes
-ceph-volume lvm batch /dev/sd[m-r] /dev/sdac --yes
-ceph-volume lvm batch /dev/sd[s-x] /dev/sdad --yes
