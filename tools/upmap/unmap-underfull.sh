27a36710b1a8c0fb9ad9cd04d49ead906779ea0a
Dan van der Ster <daniel.vanderster@cern.ch>
2020-02-13 12:06:48 +0100

    Remove obsolete unmap-underfull.sh

--- tools/upmap/unmap-underfull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-UNDERFULL=`ceph osd df | grep hdd | grep 1.00000 | sort -k8 -n | head -n4 | awk '{print $1}'`
-
-for osd in $UNDERFULL
-do
-#  echo Unmapping $osd ...
-  ceph osd dump | grep pg_upmap_items | egrep "\b${osd}\b" | awk '{print "ceph osd rm-pg-upmap-items", $2, "&"}' | sort -R | head -n4
-done
