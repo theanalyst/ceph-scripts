c01ad64e7c8f5ebfcbf46304cb072063e04530e6
Dan van der Ster <daniel.vanderster@cern.ch>
2020-05-20 13:30:38 +0200

    offline-compact-slow.sh: one at a time

---
+++ tools/bluestore/offline-compact-slow.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -x
+
+ceph osd set noout
+ceph osd set noin
+for OSD in /var/lib/ceph/osd/ceph-*;
+do
+  ID=`cat ${OSD}/whoami`
+  ceph osd ok-to-stop ${ID}
+  systemctl stop ceph-osd@${ID}
+  sleep 10
+  ceph-kvstore-tool bluestore-kv ${OSD} compact &> /var/log/ceph/compact.${ID}.log
+  sleep 5
+  systemctl start ceph-osd@${ID}
+done
+ceph osd unset noout
+ceph osd unset noin
