4909d9733e2fa133bb9a15a8d1ff10342519f7e2
Julien Collet <julien.collet@cern.ch>
2020-05-28 18:27:01 +0200

    CEPH-899: add history/dmesg reports

--- tools/ceph-disk-replacement/no-osd-left-behind.sh
+++ tools/ceph-disk-replacement/no-osd-left-behind.sh
@@ -20,7 +20,23 @@ do
     echo "Checking $i"
     for j in `ceph --cluster $1 device ls-by-daemon --format=json-pretty $i | jq -c '.[] | .location[] | { host: .host, dev: .dev}'`;
     do
-        echo $j
+        target_host=`echo $j  | jq -r -c '.host'`
+        target_drive=`echo $j | jq -r -c '.dev'`
+
+        dmesg_report=`ssh $target_host dmesg -T | grep $target_drive | grep "medium error" | tail -1`
+        if [[ ! -z $dmesg_report ]];
+        then 
+            echo "[$target_host:$target_drive] Latest dmesg error:"
+            echo "[$target_host:$target_drive]       $dmesg_report" 
+        fi
+        
+        hist_report=`ssh $target_host cat /root/.bash_history | grep -E "\-\-dev \/dev\/$target_drive"`
+        if [[ ! -z $hist_report ]];
+        then
+            echo "[$target_host:$target_drive] Plausible repair in progress..."
+            echo "[$target_host:$target_drive] $target_drive has `ssh $target_host smartctl -a /dev/$target_drive -j | jq '.power_on_time | .[]' ` power on hours on the clock"
+            echo "[$target_host:$target_drive] $hist_report"
+        fi
     done 
 done
     
