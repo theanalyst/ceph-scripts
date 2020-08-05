d9b8045fcdce538faef621ba82d17fd02dac9732
Julien COLLET <julien.collet@cern.ch>
2020-08-05 14:43:18 +0200

    translate uuid into dev

--- tools/ceph-disk-replacement/scan.sh
+++ tools/ceph-disk-replacement/scan.sh
@@ -7,7 +7,7 @@ journals=`blkid | grep journal | awk 'BEGIN {FS="="} {print $3}' | tr -d "\""`
 for line in $journals; do
     if [ `grep $line /var/lib/ceph/osd/ceph-*/journal_uuid | wc -l` -eq 0 ]; then
         if [ `lvs -odevices,tags | grep $line | wc -l` -eq 0 ]; then
-            echo $line
+            blkid | grep $line | sed -e 's/:.*$//'
         fi
     fi
 done
