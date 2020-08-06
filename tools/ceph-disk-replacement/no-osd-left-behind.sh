37bf3fc59d52d9475bc0f985e08fceb846e637be
Julien COLLET <julien.collet@cern.ch>
2020-08-06 15:56:15 +0200

    fix no osd left behind

--- tools/ceph-disk-replacement/no-osd-left-behind.sh
+++ tools/ceph-disk-replacement/no-osd-left-behind.sh
@@ -11,32 +11,38 @@ then
   exit -1
 fi
 
+
+
 # Check for down osds
-for i in `ceph --cluster $1 osd tree down | grep -Eo "osd.[0-9]+"`; 
+for i in `ceph --cluster $1 osd tree down | grep -Eo "osd.[0-9]+"`;
 do
 #    # Checking each drives of the down osds
     echo "Checking $i"
-    if [ `` ]
     for j in `ceph --cluster $1 device ls-by-daemon --format=json-pretty $i | jq -c '.[] | .location[] | { host: .host, dev: .dev}'`;
     do
         target_host=`echo $j  | jq -r -c '.host'`
         target_drive=`echo $j | jq -r -c '.dev'`
 
-        dmesg_report=`ssh $target_host dmesg -T | grep $target_drive | grep "medium error" | tail -1`
+        dmesg_report=`ssh -oStrictHostKeyChecking=no $target_host dmesg -T | grep $target_drive | grep "medium error" | tail -1`
         if [[ ! -z $dmesg_report ]];
-        then 
+        then
             echo "[$target_host:$target_drive] Latest dmesg error:"
             echo "[$target_host:$target_drive]       $dmesg_report" 
         fi
-        
-        hist_report=`ssh $target_host cat /root/.bash_history | grep -E "\-\-dev \/dev\/$target_drive"`
+
+        hist_report=`ssh -oStrictHostKeyChecking=no $target_host cat /root/.bash_history | grep -E "\-\-dev \/dev\/$target_drive"`
         if [[ ! -z $hist_report ]];
         then
             echo "[$target_host:$target_drive] Plausible repair in progress..."
-            echo "[$target_host:$target_drive] $target_drive has `ssh $target_host smartctl -a /dev/$target_drive -j | jq '.power_on_time | .[]' ` power on hours on the clock"
-            echo "[$target_host:$target_drive] history:"
-            echo "$hist_report"
+            diskreport=`ssh -oStrictHostKeyChecking=no $target_host smartctl -a /dev/$target_drive -j`;
+            if [ `echo $diskreport | grep "No such device" -c` -eq 0 ];
+            then
+                echo "[$target_host:$target_drive] $target_drive has `echo $diskreport | jq '.power_on_time | .[]' ` power on hours on the clock"
+            else
+                echo "[$target_host:$target_drive] Drive appears to be missing"
+            fi
+            echo "[$target_host:$target_drive] $hist_report"
         fi
-    done 
+    done
 done
-    
+
