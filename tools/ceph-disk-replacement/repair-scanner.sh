30be83b0f53076d90bfa54bb282c27a4c1cc8778
Julien COLLET <julien.collet@cern.ch>
2020-09-01 17:07:19 +0200

    repair-scanner: initial commit

---
+++ tools/ceph-disk-replacement/repair-scanner.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#usage ./repair-scanner.sh <clustername> 
+
+
+MONITORING_HOST="filer-carbon.cern.ch"
+MONITORING)PORT="2003"
+METRIC_PREFIX="ceph-repairs"
+
+for i in `ceph osd tree | grep host | awk '{print $4}'`; 
+do  
+  echo -n "$i "; 
+  drain=`ssh $i ls /tmp/log.drain.*`;
+  replt=`ssh $i ls /tmp/log.prepare.*`;
+
+  if [ $drain -ne "" ];
+  then
+    for j in `echo $drain`;
+    do
+      echo "[draining] $i $j"
+    done
+  fi
+
+  if [ $replt -ne "" ];
+  then
+    for j in `echo $replt`;
+    do
+      echo "[replacing] $i $j"
+    done
+  fi
+done
