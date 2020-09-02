ec86d3c8de0348aa1e536622d60792a5d45245ac
Julien COLLET <julien.collet@cern.ch>
2020-09-02 18:30:27 +0200

    generate-prepare-metrics: initial commit

---
+++ tools/ceph-disk-replacement/generate-prepare-metrics.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cluster=`facter hostgroup_1`
+
+
+for disk in `ls /tmp/log.prepare.*`;
+do
+  echo "cephrepairs.${cluster}.`echo $disk | sed -e 's/\/tmp\/log.//' -e 's/.cern.ch//'` 1 `date +%s`" | nc filer-carbon.cern.ch 2003;
+done
