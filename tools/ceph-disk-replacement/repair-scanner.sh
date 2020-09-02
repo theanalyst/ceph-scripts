c218cc7bb398f22ffd9143efce6dcbf540f4989a
Julien COLLET <julien.collet@cern.ch>
2020-09-02 15:13:10 +0200

    add timeout to repair-scanner

--- tools/ceph-disk-replacement/repair-scanner.sh
+++ tools/ceph-disk-replacement/repair-scanner.sh
@@ -9,8 +9,8 @@ METRIC_PREFIX="ceph-repairs"
 
 for i in `ceph osd tree | grep host | awk '{print $4}'`; 
 do
-  drain=`ssh -oStrictHostKeyChecking=no $i ls /tmp/log.drain.* 2> /dev/null`;
-  replt=`ssh -oStrictHostKeyChecking=no $i ls /tmp/log.prepare.* 2> /dev/null`;
+  drain=`timeout 10 ssh -oStrictHostKeyChecking=no $i ls /tmp/log.drain.* 2> /dev/null`;
+  replt=`timeout 10 ssh -oStrictHostKeyChecking=no $i ls /tmp/log.prepare.* 2> /dev/null`;
 
   if [ ! -z $drain ];
   then
