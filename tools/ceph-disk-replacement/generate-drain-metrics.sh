0946aab6d92b1b920feee3005c873581adb7a4b2
Julien COLLET <julien.collet@cern.ch>
2020-09-02 16:21:58 +0200

    generate-drain-metrics.sh: initial commit

---
+++ tools/ceph-disk-replacement/generate-drain-metrics.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$cluster=`facter hostgroup1`
+
+
+for disk in `ls /tmp/log.drain.*`;
+do
+  echo "cephrepairs.${cluster}.`echo $i | sed -e 's/\/tmp\/log.//' -e 's/.cern.ch//'` 1 `date +%s`"
+  echo "cephrepairs.${cluster}.`echo $i | sed -e 's/\/tmp\/log.//' -e 's/.cern.ch//'` 1 `date +%s`" | nc filer-carbon.cern.ch 2003;
+done
