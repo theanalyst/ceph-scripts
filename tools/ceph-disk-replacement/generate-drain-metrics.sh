aefc747128f6e404179b92273e1a05ed03c38ed8
Julien COLLET <julien.collet@cern.ch>
2020-09-02 16:26:43 +0200

    remove unnecessary echo

--- tools/ceph-disk-replacement/generate-drain-metrics.sh
+++ tools/ceph-disk-replacement/generate-drain-metrics.sh
@@ -5,6 +5,5 @@ cluster=`facter hostgroup_1`
 
 for disk in `ls /tmp/log.drain.*`;
 do
-  echo "cephrepairs.${cluster}.`echo $i | sed -e 's/\/tmp\/log.//' -e 's/.cern.ch//'` 1 `date +%s`"
-  echo "cephrepairs.${cluster}.`echo $i | sed -e 's/\/tmp\/log.//' -e 's/.cern.ch//'` 1 `date +%s`" | nc filer-carbon.cern.ch 2003;
+  echo "cephrepairs.${cluster}.`echo $disk | sed -e 's/\/tmp\/log.//' -e 's/.cern.ch//'` 1 `date +%s`" | nc filer-carbon.cern.ch 2003;
 done
