bb613ea64de590f1672987f3bc4cb4f66ff15ff7
Julien COLLET <julien.collet@cern.ch>
2020-09-03 10:00:50 +0200

    fuse generate metrics

---
+++ tools/ceph-disk-replacement/generate-metrics.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cluster=`/opt/puppetlabs/bin/facter hostgroup_1`
+
+
+for disk in `ls /tmp/log.prepare.*`;
+do
+  echo "cephrepairs.${cluster}.`echo $disk | sed -e 's/\/tmp\/log.//' -e 's/.cern.ch//'` 1 `date +%s`" | nc filer-carbon.cern.ch 2003;
+done
+
+for disk in `ls /tmp/log.drain.*`;
+do
+  echo "cephrepairs.${cluster}.`echo $disk | sed -e 's/\/tmp\/log.//' -e 's/.cern.ch//'` 1 `date +%s`" | nc filer-carbon.cern.ch 2003;
+done
