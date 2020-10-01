075f89ed82a69388e9f9858330873fba1969ef27
Julien Collet <julien.collet@cern.ch>
2020-10-01 15:02:23 +0200

    change backend check value

--- tools/s3-tools/lb-traefik-health-check.sh
+++ tools/s3-tools/lb-traefik-health-check.sh
@@ -2,7 +2,7 @@
 
 num_backends=`curl -s -u $1:$2 http://localhost/traefik/api/providers/consul_catalog/backends | jq '.' | grep backend | wc -l`
 
-if [[ $num_backends > 0 ]];
+if [[ $num_backends > 1 ]];
 then
     exit 0
 else
