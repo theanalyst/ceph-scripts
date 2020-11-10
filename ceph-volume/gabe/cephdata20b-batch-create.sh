be27e93e3d32c5679b949881495e2a6a52219d7a
Dan van der Ster <daniel.vanderster@cern.ch>
2020-11-10 10:16:36 +0100

    cephdata20b-batch-create: typo

--- ceph-volume/gabe/cephdata20b-batch-create.sh
+++ ceph-volume/gabe/cephdata20b-batch-create.sh
@@ -3,4 +3,4 @@
 ceph-volume lvm batch /dev/sdc /dev/sd[g-r]
 ceph-volume lvm batch /dev/sdd /dev/sd[s-z] /dev/sda[a-d]
 ceph-volume lvm batch /dev/sde /dev/sda[d-p]
-ceph-volume lvm batch /dev/sde /dev/sda[q-z] /dev/sdb[a-b]
+ceph-volume lvm batch /dev/sdf /dev/sda[q-z] /dev/sdb[a-b]
