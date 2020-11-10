17220812f686e0f2f7ebe87800ac616a4d19e1c3
Dan van der Ster <daniel.vanderster@cern.ch>
2020-11-10 10:18:42 +0100

    cephdata20b-batch-create: use block-dbs option

--- ceph-volume/flax/cephdata20b-batch-create.sh
+++ ceph-volume/flax/cephdata20b-batch-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ceph-volume lvm batch /dev/sdc /dev/sd[g-r]
-ceph-volume lvm batch /dev/sdd /dev/sd[s-z] /dev/sda[a-d]
-ceph-volume lvm batch /dev/sde /dev/sda[d-p]
-ceph-volume lvm batch /dev/sdf /dev/sda[q-z] /dev/sdb[a-b]
+ceph-volume lvm batch /dev/sd[g-r] --block-dbs /dev/sdc
+ceph-volume lvm batch /dev/sd[s-z] /dev/sda[a-d] --block-dbs /dev/sdd
+ceph-volume lvm batch /dev/sda[d-p] --block-dbs /dev/sde
+ceph-volume lvm batch /dev/sda[q-z] /dev/sdb[a-b] --block-dbs /dev/sdf
