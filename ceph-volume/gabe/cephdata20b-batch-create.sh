fe04bd980a30983418157b7dfe810e0f2ed14398
Dan van der Ster <daniel.vanderster@cern.ch>
2020-11-10 10:15:36 +0100

    gabe: 4 ssd block.dbs

---
+++ ceph-volume/gabe/cephdata20b-batch-create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ceph-volume lvm batch /dev/sdc /dev/sd[g-r]
+ceph-volume lvm batch /dev/sdd /dev/sd[s-z] /dev/sda[a-d]
+ceph-volume lvm batch /dev/sde /dev/sda[d-p]
+ceph-volume lvm batch /dev/sde /dev/sda[q-z] /dev/sdb[a-b]
