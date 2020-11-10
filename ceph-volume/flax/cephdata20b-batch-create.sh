9794609dbda088e202dd6bd6425b11eff28118fd
Dan van der Ster <daniel.vanderster@cern.ch>
2020-11-10 10:14:43 +0100

    flax: 4 block.dbs

--- ceph-volume/flax/cephdata20b-batch-create.sh
+++ ceph-volume/flax/cephdata20b-batch-create.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# use the first (of four) ssds as an osd
-ceph-volume lvm batch /dev/sdc
-
-# use the remaining 3 ssds as block.db for the 48 hdds
-ceph-volume lvm batch /dev/sdd /dev/sd[g-v]
-ceph-volume lvm batch /dev/sde /dev/sd[w-z] /dev/sda[a-l]
-ceph-volume lvm batch /dev/sdf /dev/sda[m-z] /dev/sdb[a-b]
+ceph-volume lvm batch /dev/sdc /dev/sd[g-r]
+ceph-volume lvm batch /dev/sdd /dev/sd[s-z] /dev/sda[a-d]
+ceph-volume lvm batch /dev/sde /dev/sda[d-p]
+ceph-volume lvm batch /dev/sde /dev/sda[q-z] /dev/sdb[a-b]
