9fa3509cdb756c0baf519f3620d32d6ab794d24a
Dan van der Ster <daniel.vanderster@cern.ch>
2020-11-16 13:54:32 +0100

    online-compact: print the slow_used_bytes

--- tools/bluestore/online-compact.sh
+++ tools/bluestore/online-compact.sh
@@ -3,6 +3,8 @@
 for OSD in /var/lib/ceph/osd/ceph-*/whoami;
 do
   ID=`cat ${OSD}`
-  echo ceph daemon osd.${ID} compact
-  echo ceph daemon osd.${ID} compact
+  echo osd.$ID slow_used_bytes before: $(ceph daemon osd.$ID perf dump bluefs | jq .bluefs.slow_used_bytes)
+  ceph daemon osd.$ID compact
+  ceph daemon osd.$ID compact
+  echo osd.$ID slow_used_bytes after: $(ceph daemon osd.$ID perf dump bluefs | jq .bluefs.slow_used_bytes)
 done
