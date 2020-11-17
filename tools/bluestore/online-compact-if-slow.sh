627b18c85f87918477d92e6c6df6ea9875056a1d
Dan van der Ster <daniel.vanderster@cern.ch>
2020-11-17 08:30:53 +0100

    online-compact: add -n dry run option

---
+++ tools/bluestore/online-compact-if-slow.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+dry_run=0
+
+while getopts 'n' opt; do
+    case "$opt" in
+        n) dry_run=1 ;;
+        *) echo 'error in command line parsing' >&2
+           exit 1
+    esac
+done
+
+if [ "$dry_run" -eq 1 ]; then
+    cmd=echo
+else
+    cmd=''
+fi
+
+for OSD in /var/lib/ceph/osd/ceph-*/whoami;
+do
+  ID=`cat ${OSD}`
+  BEFORE=$(ceph daemon osd.$ID perf dump bluefs | jq .bluefs.slow_used_bytes)
+  echo osd.$ID slow_used_bytes before: $BEFORE
+  if (( $BEFORE > 0 ))
+  then
+    $cmd ceph daemon osd.$ID compact
+    $cmd ceph daemon osd.$ID compact
+    AFTER=$(ceph daemon osd.$ID perf dump bluefs | jq .bluefs.slow_used_bytes)
+    echo osd.$ID slow_used_bytes after: $AFTER
+  fi
+done
