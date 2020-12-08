cc140c57593bf09ae475fdc7f2f3e7fa9a6a05ed
Julien COLLET <julien.collet@cern.ch>
2020-12-08 10:07:53 +0100

    send ceph-disk-repair metrics to metrictank

--- tools/ceph-disk-replacement/generate-metrics.sh
+++ tools/ceph-disk-replacement/generate-metrics.sh
@@ -5,3 +5,7 @@ cluster=`/opt/puppetlabs/bin/facter hostgroup_1`
 
 echo "cephrepairs.${cluster}.drain.`hostname -s` `ls /root/log.${cluster}.drain.* 2> /dev/null | wc -l` `date +%s`" | nc filer-carbon.cern.ch 2003;
 echo "cephrepairs.${cluster}.prepare.`hostname -s` `ls /root/log.${cluster}.prepare.* 2> /dev/null | wc -l` `date +%s`" | nc filer-carbon.cern.ch 2003;
+
+
+echo "cephrepairs.${cluster}.drain.`hostname -s` `ls /root/log.${cluster}.drain.* 2> /dev/null | wc -l` `date +%s`" | nc metrictank-carbon.cern.ch 2003;
+echo "cephrepairs.${cluster}.prepare.`hostname -s` `ls /root/log.${cluster}.prepare.* 2> /dev/null | wc -l` `date +%s`" | nc metrictank-carbon.cern.ch 2003;
