65be4c0629d3ef050d94d88494971e2f8baeb04c
Enrico Bocchi <enrico.bocchi@cern.ch>
2021-01-25 16:52:13 +0100

    s3-health-cron: Update for ebocchi's telegram

--- tools/s3-tools/s3-health-cron.sh
+++ tools/s3-tools/s3-health-cron.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 
-fping s3.cern.ch > /dev/null;
-RETVAL=$?;
 
-if [ $RETVAL -ne 0 ];
-then
-  STATUS="offline"
+HOSTNAME='s3.cern.ch'
+TIMEOUT=3
+TELEGRAM_SEND='/afs/cern.ch/user/e/ebocchi/.local/bin/telegram-send'
+
+
+timeout $TIMEOUT fping $HOSTNAME > /dev/null 2>&1
+RETVAL=$?
+
+if [ $RETVAL -ne 0 ]; then
+  $TELEGRAM_SEND "Unable to ping $HOSTNAME. Please check!"
 else
-  s3cmd ls > /dev/null;
-  s3retval=$?
-  
-  hostcount=`host s3.cern.ch | grep "has address" | wc -l`;
-  
-  if [ $s3retval -ne 0 ]; then
-    /afs/cern.ch/user/j/jcollet/.local/bin/telegram-send "CERN Ceph/S3 service seems down (${hostcount} hosts)" 
+  timeout $TIMEOUT s3cmd ls > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    $TELEGRAM_SEND "Unable to \`s3cmd ls\`. S3 service seems down. Please check!"
   fi
 fi 
 
