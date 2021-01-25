21d23a095cc397670e9a4308ef732b330bca417d
Enrico Bocchi <enrico.bocchi@cern.ch>
2021-01-25 16:52:22 +0100

    s3-host-count: Update for ebocchi's telegram

--- tools/s3-tools/s3-host-count.sh
+++ tools/s3-tools/s3-host-count.sh
@@ -1,18 +1,23 @@
-#!/usr/bin/env bash
+#!/usr/bash
 
+HOSTNAME='s3.cern.ch'
+HOSTCOUNT_THRESHOLD=4
+TIMEOUT=3
+TELEGRAM_SEND='/afs/cern.ch/user/e/ebocchi/.local/bin/telegram-send'
 
-fping s3.cern.ch > /dev/null;
-RETVAL=$?;
+timeout $TIMEOUT fping $HOSTNAME > /dev/null 2>&1
+RETVAL=$?
 
-if [ $RETVAL -ne 0 ];
-then
-  echo " "
+if [ $RETVAL -ne 0 ]; then
+  $TELEGRAM_SEND "Unable to ping $HOSTNAME. Please check!"
 else
-  HOSTCOUNT=`host s3.cern.ch | grep "has address" | wc -l`;
- 
-  if [ $HOSTCOUNT -le 4 ];
-  then
-    /afs/cern.ch/user/j/jcollet/.local/bin/telegram-send "Only ${HOSTCOUNT} hosts in s3.cern.ch"
+  IPv4=$(timeout $TIMEOUT host $HOSTNAME | grep "has address" | wc -l)
+  IPv6=$(timeout $TIMEOUT host $HOSTNAME | grep "has IPv6 address" | wc -l)
+
+  if [ $IPv4 -le $HOSTCOUNT_THRESHOLD ]; then
+    $TELEGRAM_SEND "$HOSTNAME has only $IPv4 IPv4 addresses in the alias!"
+  fi
+  if [ $IPv6 -le $HOSTCOUNT_THRESHOLD ]; then
+    $TELEGRAM_SEND "$HOSTNAME has only $IPv6 IPv6 addresses in the alias!"
   fi
 fi
-
