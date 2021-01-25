2fb4650152d1b982d3ecd8b63879c4246f751ca6
Julien COLLET <julien.collet@cern.ch>
2021-01-25 12:16:16 +0100

    ceph-disk-replacement: cleanup

--- tools/ceph-disk-replacement/list-available-drives.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-#drives=`ceph-volume inventory | awk '{ if( $4 == "True" && $5 == "True") print $0}' | grep -v "/dev/m" `
-drives=`ceph-volume inventory --format json | jq --arg host ${HOSTNAME} '.[] | select(.available) | . += { hostname:$host}'`
-echo "$drives"
