0cf6b063fe8b4cfeba0675cc86f9773f243caff5
Dan van der Ster <daniel.vanderster@cern.ch>
2021-03-25 14:39:49 +0100

    reboot-mon: skip ipmitool on vms

--- cern/rebooting/reboot-mon.sh
+++ cern/rebooting/reboot-mon.sh
@@ -6,12 +6,16 @@ echo Checking if it is OK to stop
 HOSTNAME=$(hostname -s)
 ceph mon ok-to-stop mon.`hostname -s`
 
-echo 
-echo Resetting BMC...
-ipmitool mc reset cold
+if facter -p is_virtual | grep -q false
+then
+    echo
+    echo Resetting BMC...
+    ipmitool mc reset cold
+fi
 
 echo
 echo Disabling no_contact alarm...
+kinit -k
 roger update --nc_alarmed false --duration 30min ${HOSTNAME}
 
 echo
