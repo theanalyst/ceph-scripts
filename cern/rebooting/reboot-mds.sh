859b91202fbbde7823a7dd2f2b0041f35670867d
Dan van der Ster <daniel.vanderster@cern.ch>
2021-03-25 14:41:25 +0100

    reboot-mds: run ipmitool on physical boxes

--- cern/rebooting/reboot-mds.sh
+++ cern/rebooting/reboot-mds.sh
@@ -7,6 +7,13 @@ kinit -k
 echo Checking if it is OK to stop
 ceph daemon mds.`hostname -s` status | jq .state | grep standby
 
+if facter -p is_virtual | grep -q false
+then
+    echo
+    echo Resetting BMC...
+    ipmitool mc reset cold
+fi
+
 echo
 echo Disabling no_contact alarm...
 roger update --nc_alarmed false --duration 30min
