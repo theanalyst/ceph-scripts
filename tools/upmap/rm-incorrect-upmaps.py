a9c13513f486ff84ec288859b08e45138dd09be9
Dan van der Ster <daniel.vanderster@cern.ch>
2021-04-19 08:57:16 +0200

    rm-incorrect-upmaps: 2to3

--- tools/upmap/rm-incorrect-upmaps.py
+++ tools/upmap/rm-incorrect-upmaps.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # this script looks at all pg_upmap_items and checks if
 # any have the same osd twice in the from or to set
 
-import json, commands, sys
+import json, subprocess, sys
 
-osd_dump_json = commands.getoutput('ceph osd dump -f json')
+osd_dump_json = subprocess.getoutput('ceph osd dump -f json')
 osd_dump = json.loads(osd_dump_json)
 upmaps = osd_dump['pg_upmap_items']
 
-pgs_json = commands.getoutput('ceph pg ls -f json')
+pgs_json = subprocess.getoutput('ceph pg ls -f json')
 pgs = json.loads(pgs_json)
 
-cmd_out = commands.getoutput('ceph osd crush rule dump -f json | jq -r \'.[] as $k | "\($k.rule_id) \($k.steps[-2].type)"\'')
+cmd_out = subprocess.getoutput('ceph osd crush rule dump -f json | jq -r \'.[] as $k | "\($k.rule_id) \($k.steps[-2].type)"\'')
 crush_rule_failure_domains = dict(item.split() for item in cmd_out.split('\n'))
 
 pools = {}
@@ -21,7 +21,7 @@ for pool in osd_dump['pools']:
   pools[str(pool['pool'])] = pool #pool pool pool
 
 nodes = {}
-for node in json.loads(commands.getoutput('ceph osd tree -f json'))['nodes']:
+for node in json.loads(subprocess.getoutput('ceph osd tree -f json'))['nodes']:
   if node['id'] in nodes:
     nodes[node['id']].update(node)
   else:
@@ -57,18 +57,18 @@ for pg in upmaps:
   f = [x['from'] for x in pg['mappings']]
   t = [x['to'] for x in pg['mappings']]
   if len(f) != len(set(f)) or len(t) != len(set(t)):
-    print pgid, 'upmap has osds duplicated in the from or to set', pg['mappings']
+    print(pgid, 'upmap has osds duplicated in the from or to set', pg['mappings'])
     rm = True
   if len(f+t) != len(set(f+t)):
-    print pgid, 'upmap has osds duplicated:', pg['mappings']
+    print(pgid, 'upmap has osds duplicated:', pg['mappings'])
     rm = True
   for x in pg['mappings']:
     if x['to'] not in up[pgid]:
-      print pgid, 'upmap has "to" osds which are not in the up set', pg['mappings']
+      print(pgid, 'upmap has "to" osds which are not in the up set', pg['mappings'])
       rm = True
   failure_domain_nodes = [lookup_parent_id(osd, nodes, pools[pgid.split('.')[0]]['failure_domain']) for osd in up[pgid]]
   if len(failure_domain_nodes) != len(set(failure_domain_nodes)):
-    print pgid, 'upmap does not follow the crush rule on failure domain', pg['mappings'], up[pgid], [nodes[fdn]['name'] for fdn in failure_domain_nodes]
+    print(pgid, 'upmap does not follow the crush rule on failure domain', pg['mappings'], up[pgid], [nodes[fdn]['name'] for fdn in failure_domain_nodes])
     rm = True
   if rm:
-    print 'ceph osd rm-pg-upmap-items', pgid
+    print('ceph osd rm-pg-upmap-items', pgid)
