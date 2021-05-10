dd567e98d737866d4bc413438bfdb1a4764a766d
Dan van der Ster <daniel.vanderster@cern.ch>
2021-05-10 11:43:34 +0200

    Serial fsck for OSDs: offline-fsck-serial.sh

---
+++ tools/bluestore/offline-fsck-serial.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -x
+
+roger update --appstate=intervention --message="BlueStore FSCK OSDs"
+
+ceph osd set noout
+ceph osd set noin
+for OSD in /var/lib/ceph/osd/ceph-*;
+do
+  ID=`cat ${OSD}/whoami`
+  ceph osd ok-to-stop ${ID}
+  systemctl stop ceph-osd@${ID}
+  sleep 10
+  ceph-bluestore-tool fsck --path $OSD &> /var/log/ceph/fsck.${ID}.log
+  sleep 5
+  systemctl start ceph-osd@${ID}
+done
+ceph osd unset noout
+ceph osd unset noin
+
+roger update --appstate=production
