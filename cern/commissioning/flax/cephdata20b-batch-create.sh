085483bf2c5aa4cc0d4a2e022bcf4654e979b085
Dan van der Ster <daniel.vanderster@cern.ch>
2021-09-08 14:11:21 +0200

    flax: update the cephdata20b-batch-create.sh script

--- cern/commissioning/flax/cephdata20b-batch-create.sh
+++ cern/commissioning/flax/cephdata20b-batch-create.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-ceph-volume lvm batch /dev/sd[g-r] --db-devices /dev/sdc
-ceph-volume lvm batch /dev/sd[s-z] /dev/sda[a-d] --db-devices /dev/sdd
-ceph-volume lvm batch /dev/sda[d-p] --db-devices /dev/sde
-ceph-volume lvm batch /dev/sda[q-z] /dev/sdb[a-b] --db-devices /dev/sdf
+ceph-volume lvm batch /dev/sd[g-z] /dev/sda[a-z] /dev/sdb[a-b] --db-devices /dev/sd[c-f]
