11f5b7aa428e196ec4fd1ad58ae75aebf2b6b262
Dan van der Ster <daniel.vanderster@cern.ch>
2021-09-21 10:05:14 +0200

    add tools maskselinux.sh

---
+++ tools/maskselinux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    /usr/sbin/selinuxenabled
+    echo "/usr/sbin/selinuxenabled reports:" $?
+    echo
+    echo "Usage: $0 [--mask|--unmask]"
+    echo
+    exit 0
+fi
+
+if [ "$1" == "--mask" ]
+then
+    echo Linking to /bin/false
+    mv -f /usr/sbin/selinuxenabled /usr/bin/selinuxenabled.orig
+    ln -s /bin/false /usr/sbin/selinuxenabled
+fi
+
+if [ "$1" == "--unmask" ]
+then
+    echo Restoring backup /usr/bin/selinuxenabled
+    mv -f /usr/sbin/selinuxenabled.orig /usr/bin/selinuxenabled
+fi
