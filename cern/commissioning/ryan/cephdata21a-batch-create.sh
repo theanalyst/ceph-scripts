ce76acc6d81bf6535f9a4790e03a9742a72e7299
Enrico Bocchi <ceph-admins@cern.ch>
2021-10-13 15:59:09 +0200

    ryan: Batch create osds

--- cern/commissioning/ryan/cephdata21a-batch-create.sh
+++ cern/commissioning/ryan/cephdata21a-batch-create.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-ceph-volume lvm batch /dev/sd[a-l] --db-devices /dev/sday
-ceph-volume lvm batch /dev/sd[m-x] --db-devices /dev/sdaz
-ceph-volume lvm batch /dev/sd[y-z] /dev/sda[a-j] --db-devices /dev/sdba
-ceph-volume lvm batch /dev/sda[k-v] --db-devices /dev/sdbb
+ceph-volume lvm batch /dev/sd[a-z] /dev/sda[a-v] /dev/sda[y-z] /dev/sdba /dev/sdbb
