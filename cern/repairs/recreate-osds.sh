420fc8653c5b3d5bd7715614248c79f7f3e08cd8
Dan van der Ster <daniel.vanderster@cern.ch>
2021-12-01 14:22:39 +0100

    recreate-osds.sh: add example batch output for n00bs

--- cern/repairs/recreate-osds.sh
+++ cern/repairs/recreate-osds.sh
@@ -5,6 +5,21 @@
 # This tool looks for destroyed OSDs on the localhost and uses ceph-volume to
 # take an inventory of all usable devices. Finally, it suggests a ceph-volume
 # command to be used to recreate missing OSDs.
+#
+# The output from ceph-volume batch will show which OSD id, data, and block_db
+# devices that it will use. Here is a good example; anything else should be
+# aborted.
+#
+# Total OSDs: 1
+#
+#   Type            Path                                                    LV Size         % of device
+# ----------------------------------------------------------------------------------------------------
+#   OSD id          234
+#   data            /dev/sdh                                                5.46 TB         100.00%
+#   block_db        /dev/sdaa                                               37.26 GB        16.67%
+# --> The above OSDs would be created if the operation continues
+# --> do you want to proceed? (yes/no)
+#
 
 echo This tool is in beta testing. Double check output before running the suggested command.
 echo
