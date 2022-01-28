e1d257fccda3d5678826141024a4c65973b5060d
Enrico Bocchi <enrico.bocchi@cern.ch>
2022-01-28 13:43:16 +0100

    Script to create telemetry-proxy VM

---
+++ cern/commissioning/_other/cephtelemetry-proxy.sh
@@ -0,0 +1,31 @@
+##
+# This is to create a mini-gabe cluster for testing
+##
+
+#!/bin/bash -x
+
+unset OS_PROJECT_ID;
+unset OS_TENANT_ID;
+unset OS_TENANT_NAME;
+export OS_PROJECT_NAME="IT Ceph Storage Service";
+
+openstack server list  > /dev/null 2>&1
+if [ $? != "0" ] ; then
+  echo "openstack server list not working"
+  exit
+fi
+
+PREFIX='cephtelemetry-proxy-'
+FLAVOR='m2.small'
+#VM_ZONE="cern-geneva-a"
+#VM_ZONE="cern-geneva-b"
+#VM_ZONE="cern-geneva-c"
+
+ai-bs     --landb-mainuser ceph-admins \
+          --landb-responsible ceph-admins \
+	  --nova-sshkey ebocchi \
+          --nova-flavor $FLAVOR \
+          --cs8 \
+          --foreman-environment 'production' \
+          --foreman-hostgroup 'ceph/telemetry_proxy' \
+          --prefix $PREFIX
