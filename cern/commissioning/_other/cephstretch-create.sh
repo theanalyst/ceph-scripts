dd58f8fda0681d25be9ce699b1adea44639a2f51
Enrico Bocchi <enrico.bocchi@cern.ch>
2022-05-19 11:48:08 +0200

    commission-stretch: Convenience script to create 5 VMs across AZs

---
+++ cern/commissioning/_other/cephstretch-create.sh
@@ -0,0 +1,40 @@
+##
+# This is to create a Ceph stretch cluster for testing
+##
+
+#!/bin/bash -x
+
+
+create_vm () {
+  local VM_NAME=$1
+  local AZ=$2
+
+  ai-bs     --landb-mainuser ceph-admins \
+            --landb-responsible ceph-admins \
+            --nova-flavor $FLAVOR \
+            --cs8 \
+            --foreman-environment 'production' \
+            --foreman-hostgroup 'ceph/spare' \
+	    --nova-availabilityzone $AZ \
+	    $VM_NAME
+}
+
+unset OS_PROJECT_ID;
+unset OS_TENANT_ID;
+unset OS_TENANT_NAME;
+export OS_PROJECT_NAME="Ceph Development";
+
+#openstack server list  > /dev/null 2>&1
+#if [ $? != "0" ] ; then
+#  echo "openstack server list not working"
+#  exit
+#fi
+
+NAME_PREFIX='cephstretch'
+FLAVOR='m2.large'
+
+create_vm $NAME_PREFIX-a-1 cern-geneva-a
+create_vm $NAME_PREFIX-a-2 cern-geneva-a
+create_vm $NAME_PREFIX-b-1 cern-geneva-b
+create_vm $NAME_PREFIX-b-2 cern-geneva-b
+create_vm $NAME_PREFIX-c-1 cern-geneva-c
