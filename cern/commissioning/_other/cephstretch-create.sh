1d1a72fdeddab76e7f7b0aee8f627ceaabca37cb
Enrico Bocchi <enrico.bocchi@cern.ch>
2022-08-10 12:34:12 +0200

    commission-stretch: Create also the volumes

--- cern/commissioning/_other/cephstretch-create.sh
+++ cern/commissioning/_other/cephstretch-create.sh
@@ -19,22 +19,40 @@ create_vm () {
 	    $VM_NAME
 }
 
+create_volume () {
+  local VOLUME_NAME=$1
+  local AZ=$2
+
+  openstack volume create \
+	  --size 100 \
+	  --type standard \
+	  --availability-zone $AZ \
+	  $VOLUME_NAME
+}
+
 unset OS_PROJECT_ID;
 unset OS_TENANT_ID;
 unset OS_TENANT_NAME;
 export OS_PROJECT_NAME="Ceph Development";
 
-#openstack server list  > /dev/null 2>&1
-#if [ $? != "0" ] ; then
-#  echo "openstack server list not working"
-#  exit
-#fi
+openstack server list  > /dev/null 2>&1
+if [ $? != "0" ] ; then
+  echo "openstack server list not working"
+  exit
+fi
 
 NAME_PREFIX='cephstretch'
 FLAVOR='m2.large'
 
 create_vm $NAME_PREFIX-a-1 cern-geneva-a
 create_vm $NAME_PREFIX-a-2 cern-geneva-a
+create_vm $NAME_PREFIX-a-3 cern-geneva-a
 create_vm $NAME_PREFIX-b-1 cern-geneva-b
 create_vm $NAME_PREFIX-b-2 cern-geneva-b
 create_vm $NAME_PREFIX-c-1 cern-geneva-c
+
+create_volume $NAME_PREFIX-a-1 ceph-geneva-1
+create_volume $NAME_PREFIX-a-2 ceph-geneva-1
+create_volume $NAME_PREFIX-a-3 ceph-geneva-1
+create_volume $NAME_PREFIX-b-1 ceph-geneva-2
+create_volume $NAME_PREFIX-b-2 ceph-geneva-2
