5f85885d61ff69c6a3cfce855486ed53aa9a5f32
Enrico Bocchi <enrico.bocchi@cern.ch>
2023-02-17 14:09:33 +0100

    commisioning: VM for krbd tests

---
+++ cern/commissioning/_other/cephkrbd-cs8.sh
@@ -0,0 +1,29 @@
+## 
+# This is to create a VM to test krbd
+#
+##
+
+#!/bin/bash -x
+
+unset OS_PROJECT_ID;
+unset OS_TENANT_ID;
+unset OS_TENANT_NAME;
+export OS_PROJECT_NAME="IT Ceph Storage Service";
+
+openstack server list  > /dev/null 2>&1
+if [ $? != "0" ] ; then
+  echo "openstack server list not working"
+  exit
+fi
+
+PREFIX='cephkrbd-testcs8-'
+FLAVOR='m2.small'
+
+ai-bs     --landb-mainuser ceph-admins \
+          --landb-responsible ceph-admins \
+	  --nova-flavor $FLAVOR \
+	  --cs8 \
+	  --nova-sshkey 'ebocchi' \
+          --foreman-environment 'qa' \
+          --foreman-hostgroup 'ceph/test/krbd' \
+          --prefix $PREFIX
