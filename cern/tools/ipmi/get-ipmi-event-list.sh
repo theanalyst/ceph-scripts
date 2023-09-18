40e534f77fb3d5d91a9699fd1591bfd7ca0c30eb
Enrico Bocchi <enrico.bocchi@cern.ch>
2023-09-18 10:03:08 +0200

    get-ipmi-event-list: support BMC using Supermicro Redfish

--- cern/tools/ipmi/get-ipmi-event-list.sh
+++ cern/tools/ipmi/get-ipmi-event-list.sh
@@ -16,7 +16,21 @@ fi
 
 echo
 echo "Your ipmi sel list command is:"
-openstack console url show -f json $HOST | \
-  stdbuf -i0 -o0 -e0 jq -r ".url.ipmitool_chassis_power_status" | \
-  stdbuf -i0 -o0 -e0 sed -e 's/chassis power status/sel list/g'
+CONSOLE=$(openstack console url show -f json $HOST)
 
+POWER_STATUS=$(echo $CONSOLE | jq -r ".url.ipmitool_chassis_power_status")
+if [ x"$POWER_STATUS" != x"null" ]; then
+  echo $POWER_STATUS | \
+    stdbuf -i0 -o0 -e0 sed -e 's/chassis power status/sel list/g'
+fi
+
+# In case BMC uses Supermicro Redfish
+POWER_STATUS=$(echo $CONSOLE | jq -r ".url.power_status")
+if [ x"$POWER_STATUS" != x"null" ]; then
+  echo $POWER_STATUS | \
+    stdbuf -i0 -o0 -e0 sed -e 's/^rf_power_reset.py /ipmitool -I lanplus /g' | \
+    stdbuf -i0 -o0 -e0 sed -e 's/ --user / -U /g' | \
+    stdbuf -i0 -o0 -e0 sed -e 's/ --password / -P /g' | \
+    stdbuf -i0 -o0 -e0 sed -e 's# --rhost https://# -H #g' | \
+    stdbuf -i0 -o0 -e0 sed -e 's/ --info/ sel list/g'
+fi
