9c2c8e46d0a04b74357670a968066c9217d98b78
Enrico Bocchi <enrico.bocchi@cern.ch>
2023-11-06 18:22:39 +0100

    miniflax: Add script to create MDS VMs

---
+++ cern/commissioning/_other/miniflax-create-mds.sh
@@ -0,0 +1,30 @@
+##
+# This is to create a mini-gabe cluster for testing
+##
+
+#!/bin/bash -x
+
+unset OS_PROJECT_ID;
+unset OS_TENANT_ID;
+unset OS_TENANT_NAME;
+export OS_PROJECT_NAME="Ceph Development";
+
+openstack server list  > /dev/null 2>&1
+if [ $? != "0" ] ; then
+  echo "openstack server list not working"
+  exit
+fi
+
+PREFIX='miniflax-mds-'
+FLAVOR='m2.large'
+VM_ZONE="cern-geneva-a"
+#VM_ZONE="cern-geneva-b"
+#VM_ZONE="cern-geneva-c"
+
+ai-bs     --landb-mainuser ceph-admins \
+          --landb-responsible ceph-admins \
+          --nova-flavor $FLAVOR \
+          --rhel8 \
+          --foreman-environment 'production' \
+          --foreman-hostgroup 'ceph/miniflax/mds' \
+          --prefix $PREFIX
