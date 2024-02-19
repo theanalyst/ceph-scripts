5c7c3de7254933d02ccfb7fc3fc4c01c06e46bf4
Enrico Bocchi <enrico.bocchi@cern.ch>
2024-02-19 12:49:12 +0100

    upmap: Sanitize json with jq before python parsing

--- tools/upmap/upmap-remapped.py
+++ tools/upmap/upmap-remapped.py
@@ -38,8 +38,8 @@
 import json, subprocess, sys
 
 try:
-  OSDS = json.loads(subprocess.getoutput('ceph osd ls -f json'))
-  DF = json.loads(subprocess.getoutput('ceph osd df -f json | jq .nodes'))
+  OSDS = json.loads(subprocess.getoutput('ceph osd ls -f json | jq -r'))
+  DF = json.loads(subprocess.getoutput('ceph osd df -f json | jq -r .nodes'))
 except ValueError:
   eprint('Error loading OSD IDs')
   sys.exit(1)
@@ -84,7 +84,7 @@ def rm_upmap_pg_items(pgid):
 
 # discover remapped pgs
 try:
-  remapped_json = subprocess.getoutput('ceph pg ls remapped -f json')
+  remapped_json = subprocess.getoutput('ceph pg ls remapped -f json | jq -r')
   remapped = json.loads(remapped_json)
 except ValueError:
   eprint('Error loading remapped pgs')
@@ -99,7 +99,7 @@ except KeyError:
   sys.exit(0)
 
 # discover existing upmaps
-osd_dump_json = subprocess.getoutput('ceph osd dump -f json')
+osd_dump_json = subprocess.getoutput('ceph osd dump -f json | jq -r')
 osd_dump = json.loads(osd_dump_json)
 upmaps = osd_dump['pg_upmap_items']
 
