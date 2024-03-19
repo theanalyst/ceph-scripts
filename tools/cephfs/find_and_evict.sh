94ee4504924c09ce8545d2dd5be11c70c8502dcb
Enrico Bocchi <enrico.bocchi@cern.ch>
2024-03-19 10:39:19 +0100

    tools-cephfs: Find and evicts prints debug only at the end

--- tools/cephfs/find_and_evict.sh
+++ tools/cephfs/find_and_evict.sh
@@ -8,16 +8,25 @@ then
   echo "No inode provided."
 fi
 
+
 # Find the path from the inode
-path=$(ceph daemon mds.`hostname -s` dump inode $INODE | jq -r .path | cut -d '/' -f 4)
-echo "  Debug -- path: $path"
+dump_inode=$(ceph daemon mds.`hostname -s` dump inode $INODE)
+fullpath=$(echo $dump_inode | jq -r .path)
+sharepath=$(echo $dump_inode | jq -r .path | cut -d '/' -f 4)
 
 # Echo a command to evict all the clients mounting that path
-echo $client_ls | jq -r '.[] | "\(.id), \(.client_metadata.root)"' | grep $path | cut -d ',' -f 1 | xargs -i echo ceph tell mds.* client evict id={}
+client_ls=$(ceph daemon mds.`hostname -s` client ls)
+echo $client_ls | jq -r '.[] | "\(.id), \(.client_metadata.root)"' | grep $sharepath | cut -d ',' -f 1 | xargs -i echo ceph tell mds.* client evict id={}
+
+# Dump the inode description from MDS
+dump_inode_file="$INODE-dumpinode-$(date +%s)"
+echo "  Debug -- writing dump_inode output to $dump_inode_file"
+echo $dump_inode > $dump_inode_file
 
 # Dump the list of clients
-client_ls=$(ceph daemon mds.`hostname -s` client ls)
-output_file="find_and_evict_clientls_$INODE_$(date +%s)"
+output_file="$INODE-clientls-$(date +%s)"
 echo "  Debug -- writing client_ls output to $output_file"
 echo $client_ls > $output_file
 
+# Dump the fullpath of the stuck operation
+echo "  Debug -- fullpath: $fullpath"
