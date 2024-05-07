efef55dbb5e742b54af4473d6ed72823f4ad3d6e
Enrico Bocchi <enrico.bocchi@cern.ch>
2024-05-07 08:50:32 +0200

    commissioning: Revamp toby mon scripts

--- cern/commissioning/toby/cephtoby-mon513-create.sh
+++ cern/commissioning/toby/cephtoby-mon513-create.sh
@@ -10,7 +10,8 @@
 unset OS_PROJECT_ID;
 unset OS_TENANT_ID;
 unset OS_TENANT_NAME;
-export OS_PROJECT_NAME="IT Ceph Storage Service - Critical Area";
+export OS_PROJECT_NAME="IT Ceph Storage Service";
+export OS_REGION_NAME="cern";
 
 openstack server list  > /dev/null 2>&1
 if [ $? != "0" ] ; then
@@ -18,13 +19,20 @@ if [ $? != "0" ] ; then
   exit
 fi
 
-PREFIX='cephtoby-mon-barn-'
+PREFIX='cephtoby-mon-513-'
 FLAVOR='m2.large'
+
+# Define VM availability zone
+VM_ZONE="cern-geneva-a"
+#VM_ZONE="cern-geneva-b"
+#VM_ZONE="cern-geneva-c"
+
 
 ai-bs     --landb-mainuser ceph-admins \
           --landb-responsible ceph-admins \
           --nova-flavor $FLAVOR \
           --rhel9 \
+	  --nova-availabilityzone $VM_ZONE \
           --foreman-environment 'production' \
           --foreman-hostgroup 'ceph/toby/mon' \
           --prefix $PREFIX
